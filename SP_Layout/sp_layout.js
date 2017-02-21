85bec17223fb426bee287252535aaa95eaec69cd
frank <nbafrank72000@gmail.com>
2017-02-22 02:19:22 +0800

    complete relationship, continue part, and go into edit photo link page

--- SP_Layout/sp_layout.js
+++ SP_Layout/sp_layout.js
@@ -24,11 +24,11 @@ $(function(){
 	$('.ff-img').hover(function(){
 		//load img's alt to li element
 		$(this).append("<div><p>" + $(this).children('img').attr('alt') + "</p></div>");
-		$(this).children('div').fadeIn(300);
-		$(this).children('div').children('p').animate({top: '0px'}, 300);
+		$(this).children('div').fadeIn(400);
+		$(this).children('div').children('p').animate({top: '0px'}, 400);
 	}, function(){
-		$(this).children('div').fadeOut(300);
-		$(this).children('div').children('p').animate({top: '10px'}, 300, function(){ 
+		$(this).children('div').fadeOut(400);
+		$(this).children('div').children('p').animate({top: '10px'}, 400, function(){ 
 																			$(this).parent('div').remove();
 																		});
 	});
@@ -36,11 +36,27 @@ $(function(){
 		$(this).parent('.relationship-container').children('.travel-content').css('border', '10px solid #ff7f50');
 		$(this).parent('.relationship-container').children('.travel-content').css('color', '#ff7f50');
 		$(this).parent('.relationship-container').children('.travel-content').css('top', '0px');
-		$(this).parent('.relationship-container').children('.travel-content').children('.travel-content-button').append("<div>MEMORY</div>");
+		$(this).parent('.relationship-container').children('.travel-content').children('.travel-content-button').children('div').css({transform: 'rotateX(0deg)'});
 	}, function(){
 		$(this).parent('.relationship-container').children('.travel-content').css('border', '10px solid rgba(0, 0, 0, 0)');
 		$(this).parent('.relationship-container').children('.travel-content').css('color', '#2f4f4f');
 		$(this).parent('.relationship-container').children('.travel-content').css('top', '50px');
-		$(this).parent('.relationship-container').children('.travel-content').children('.travel-content-button').children('div').remove();
+		$(this).parent('.relationship-container').children('.travel-content').children('.travel-content-button').children('div').css({transform: 'rotateX(90deg)'});
+	});
+	$('.play-button-hover-area').hover(function(){
+		$(this).parent('.play-button').children('.circle').css('background-color', '#ff7f50');
+		$(this).parent('.play-button').children('.circle').css('top', '0px');
+		$(this).parent('.play-button').children('.circle').css('left', '0px');
+		$(this).parent('.play-button').children('.circle').css('padding-top', '28.5px');
+		$(this).parent('.play-button').children('.circle').css('width', '80px');
+		$(this).parent('.play-button').children('.circle').css('height', '80px');
+	}, function(){
+		$(this).parent('.play-button').children('.circle').css('background-color', '#dc143c');
+		$(this).parent('.play-button').children('.circle').css('top', '5px');
+		$(this).parent('.play-button').children('.circle').css('left', '5px');
+		$(this).parent('.play-button').children('.circle').css('padding-top', '23.5px');
+		$(this).parent('.play-button').children('.circle').css('width', '70px');
+		$(this).parent('.play-button').children('.circle').css('height', '70px');
 	});
 })
+
